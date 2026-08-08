db49ab711ca7522d4a4d468d55efe14af97f1284
Miftah Syarief <miftahsyarief@mscode-lab.local>
2026-08-08 11:58:47 +0700

    inisial windows

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/postcss';
+import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     proxy: {
